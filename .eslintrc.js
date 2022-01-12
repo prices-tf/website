3f71d1dd84dc6448bdcbf9776fbf4cbe842b30fa
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2022-01-12 20:37:44 +0100

    chore: setup dev environment

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020, // Use the latest ecmascript standard
+    sourceType: 'module', // Allows using import/export statements
+    ecmaFeatures: {
+      jsx: true, // Enable JSX since we're using React
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the react version
+    },
+  },
+  env: {
+    browser: true, // Enables browser globals like window and document
+    amd: true, // Enables require() and define() as global variables as per the amd spec.
+    node: true, // Enables Node.js global variables and Node.js scoping.
+  },
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'react',
+    'simple-import-sort',
+    'jsx-a11y',
+  ],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+import vercel from '../public/vercel.svg';
+import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
@@ -61,12 +63,18 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <Image
+              src={vercel}
+              unoptimized={true}
+              alt="Vercel Logo"
+              width={72}
+              height={16}
+            />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
