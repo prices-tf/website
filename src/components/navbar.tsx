ec316e1ccb3ece9b6a8be932880ba36327cc8ba0
Nicklas Marc Pedersen <nicklas.marc.pedersen@gmail.com>
2022-01-19 20:35:29 +0100

    feat: add links to navbar
    Added API documentation and GitHub links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -50,9 +50,20 @@ export default function Navbar() {
           }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
         >
           <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
-            <Link href="/">
-              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-blue-600 hover:text-white ">
-                Home
+            <Link href="https://api2.prices.tf/docs">
+              <a
+                target="_blank"
+                className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-blue-600 hover:text-white "
+              >
+                API documentation
+              </a>
+            </Link>
+            <Link href="https://github.com/prices-tf">
+              <a
+                target="_blank"
+                className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-blue-600 hover:text-white "
+              >
+                GitHub
               </a>
             </Link>
           </div>
